3d2a096821d89ef70db116d6d2164aa73e0cef44
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-03-11 00:55:08 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React  from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import Button from './Components/Button/Button';
+import Signup from './Pages/Register/Signup';
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Switch>
+      <Route path="/register">
+          <Signup/>
+        </Route>
         <Route path="/">
-        <Button buttonName = "tour" label = "Take a tour" />
+          <Button buttonName = "tour" label = "Take a tour" />
         </Route>
       </Switch>
     </Router>
