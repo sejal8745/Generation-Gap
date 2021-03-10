365452208ce5c4e51ab8c89984528309deafa6bd
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-03-10 12:50:43 +0530

    Routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-
-import React  from 'react'
-
+import React  from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>jgcjudgcjdw</h1>
-
-      <div>Hello</div>
-
+    <Router>
+      <Switch>
+        <Route path="/">
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/Components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.css';
+function Button({buttonName, label, onClick}) {
+  return (
+    <button
+      className={buttonName}
+      onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+export default Button;
