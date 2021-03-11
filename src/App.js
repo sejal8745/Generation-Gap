ff83582c0fafee839bfbe98e238e2ddb6b45d3b3
parulsurana <parulsurana28@gmail.com>
2021-03-12 00:42:26 +0530

    Home Page Design

--- src/App.js
+++ src/App.js
@@ -2,34 +2,22 @@ import React  from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import Home from './Pages/Home/Home';
-
-import Button from './Components/Button/Button';
-
 import Card from './Components/features-card/Card';
-import { auth, provider } from './firebase';
+
 
 function App() {
-  const login= ()=>{
-    auth.signInWithPopup(provider).catch((e)=>{alert(e.message)});
-  };
+ 
   return (
     <div className="App">
     <Router>
       <Switch>
-
-        <Route path="/Home">
+      <Route path="/home">
         <Home />
-
-        <Route path="/">
-
-        <Card/>
-        <button onClick={login}>Login</button>
-
-
-        <Button buttonName = "tour" label = "Take a tour" />
-
-
         </Route>
+          <Route path="/">
+        <Card/>
+        </Route>
+       
       </Switch>
     </Router>
     </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import OP from "../../Assets/OP.png";
-
+=import OP1 from "../../Assets/OP1.png";
+import Card from "../../Components/features-card/Card"
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import GTranslateIcon from '@material-ui/icons/GTranslate';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import NoteIcon from '@material-ui/icons/Note';
+import TvIcon from '@material-ui/icons/Tv';
+import Button from "../../Components/Button/Button";
+import { auth, provider } from '../../firebase';
 
 const Home = () => {
+    const login= ()=>{
+        auth.signInWithPopup(provider).catch((e)=>{alert(e.message)});
+      };
     return (
         <div className="Home">
+        <p>theme</p>
             <div className="HomeCard">
                 <div className="CardHeader">
                     <div className="HomeHeader">
@@ -28,9 +39,10 @@ const Home = () => {
                                 <Link to='/about' activeStyle>
                                     About Us
                             </Link>
-                                <Link to='/theme' activeStyle>
-                                    Theme
+                                <Link to='/blog' activeStyle>
+                                    Blogs
                             </Link>
+                               
                                 <Link to='/language' activeStyle>
                                     Language
                              </Link>
@@ -41,9 +53,20 @@ const Home = () => {
                     <div className="HomeBody">
                         <div className="Contentleft">
                             <p>Every Generation needs Regeneration!</p>
+                            <Button 
+                                buttonName="tour"
+                                label="Take a Tour"
+                            />
+                            <div  OnClick={login}>
+                            <Button 
+                                buttonName="tour"
+                                label="SignIn"
+                            />
+                            </div>
+                           
                         </div>
                         <div className="Image">
-                            <img className="Img" src={OP} alt="OP" />
+                            <img className="Img" src={OP1} alt="OP" />
                         </div>
                         <div className="Contentright">
                             <p>Technical ability is also important.<br></br> How technology affects the generation gap<br></br> is impacted by how well older people <br></br>can learn and
@@ -54,11 +77,26 @@ const Home = () => {
                 </div>
                 <div className="CardFooter">
                     <div className="HomeFooter">
-                        <p>dsjhudhgidkhsmnkefnnnnnnnnnn</p>
-                        <p>dsjhudhgidkhsmnkefnnnnnnnnnn</p>
-                        <p>dsjhudhgidkhsmnkefnnnnnnnnnn</p>
-                        <p>dsjhudhgidkhsmnkefnnnnnnnnnn</p>
-                        <p>dsjhudhgidkhsmnkefnnnnnnnnnn</p>
+                        <Card 
+                         icon={< PhoneInTalkIcon/>}
+                          title="Talk To Your Friend"
+                        />
+                        <Card 
+                        icon={< NoteIcon/>}
+                          title="Note Down"
+                        />
+                        <Card 
+                        icon={< GTranslateIcon/>}
+                          title="Find Word"
+                        />
+                        <Card 
+                        icon={< SportsEsportsIcon/>}
+                          title="Games"
+                        />
+                        <Card 
+                        icon={< TvIcon/>}
+                          title="News"
+                        />
                     </div>
                 </div>
 
