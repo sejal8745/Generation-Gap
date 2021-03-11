ae1dbd08b833c28f1d364ca8b33755d427f6528d
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-03-11 22:42:02 +0530

    Notes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React  from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Button from './Components/Button/Button';
-import Signup from './Pages/Register/Signup';
-
-import Card from './Components/features-card/Card';
+import Button from "./Components/Button/Button";
+import Signup from "./Pages/Register/Signup";
+import Card from "./Components/features-card/Card";
+import AddNote from "./Components/AddNote/AddNote";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-      <Route path="/register">
-          <Signup/>
-        </Route>
-        <Route path="/">
-          <Button buttonName = "tour" label = "Take a tour" />
-
-        <Button buttonName = "tour" label = "Take a tour" />
-
-          <Card/>
-
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <Signup />
+          </Route>
+          <Route path="/notes">
+            <AddNote/>
+          </Route>
+          <Route path="/">
+            <Button buttonName="tour" label="Take a tour" />
+            <Card />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
